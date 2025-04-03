44ace8bec4cf365a4ef67fa5d3b799becab97148
zippostorm <toghkklli@gmail.com>
2025-04-03 10:03:28 +0300

    DB setup

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import productRoute from "./routes/product.route.js";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -18,6 +19,26 @@ app.use(morgan("dev"));
 
 app.use("/api/products", productRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+    console.log("Connected to the database");
+  } catch (error) {
+    console.log("Error connecting to the database: ", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
