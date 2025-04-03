a87ca5b452c681e151031f0f05b8812f29aef2c8
zippostorm <toghkklli@gmail.com>
2025-04-03 13:56:18 +0300

    create all API routes for product

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,13 +1,21 @@
 import express from "express";
 import {
-  getAllProducts,
+  getProducts,
   createProduct,
+  getProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
+router.get("/", getProducts);
+router.get("/:id", getProduct);
 
 router.post("/", createProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
 
 export default router;
