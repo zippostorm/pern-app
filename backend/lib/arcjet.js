c9296ad91dc6719357a745aadc9eec4a55c5489a
zippostorm <toghkklli@gmail.com>
2025-04-03 14:47:01 +0300

    arcjet setup

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,18 @@
+import arcjet, { tokenBucket, shield, detectBot } from "@arcjet/node";
+import "dotenv/config";
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    //prevent SQL injection, XSS, CSRF attacks
+    shield({ mode: "LIVE" }),
+    detectBot({ mode: "LIVE", allow: ["CATEGORY:SEARCH_ENGINE"] }),
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import { aj } from "./lib/arcjet.js";
 
 import productRoute from "./routes/product.route.js";
 import { sql } from "./config/db.js";
@@ -16,6 +17,38 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too many requests!" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied!" });
+      } else {
+        res.status(403).json({ error: "Access denied!" });
+      }
+      return;
+    }
+
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Bot access denied!" });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoute);
 
