cfdeaf21a79f178dba1b40dddd46a5a6c64d3fab
zippostorm <toghkklli@gmail.com>
2025-04-07 11:37:23 +0300

    reinstall daisyui. Fix theme bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,10 @@ import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme="forest"
+    >
       <Navbar />
 
       <Routes>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCartIcon } from "lucide-react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
+      <div className="max-w-7xl">
+        <div className="navbar px-4 min-h-[4rem] justify-between">
+          {/* LOGO */}
+          <div className="flex-1 lg:flex-none">
+            <Link to="/" className="hover:opacity-80 transition-opacity">
+              <div className="flex items-center gap-2">
+                <ShoppingCartIcon className="size-9 text-primary" />
+                <span
+                  className="font-semibold font-mono tracking-widest text-2xl 
+                    bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
+                >
+                  POSGRESTORE
+                </span>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
