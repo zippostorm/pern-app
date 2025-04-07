f865cc74a247a3101ce979dd5198d5263958e824
zippostorm <toghkklli@gmail.com>
2025-04-07 10:57:09 +0300

    create pages and components. Setup routes for it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
