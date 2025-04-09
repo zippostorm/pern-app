f905e699a1ead80b7ea091634580501f195a6ac2
zippostorm <toghkklli@gmail.com>
2025-04-09 11:50:52 +0300

    fix versions

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ app.use(async (req, res, next) => {
 app.use("/api/products", productRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
